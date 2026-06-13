f516ffa8aefef9d7b7be693e447ffdc16a50d7a7
sidharthcodes-arch <pradhansidharth73@gmail.com>
2026-06-13 12:23:52 +0530

    avoiding, railway.toml

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -34,16 +34,28 @@ import Pusher from 'pusher-js';
 | });
 */
 
-window.Pusher = Pusher;
+// window.Pusher = Pusher;
 
+// window.Echo = new Echo({
+//     broadcaster: 'reverb',
+//     key: import.meta.env.VITE_REVERB_APP_KEY,
+//     wsHost: import.meta.env.VITE_REVERB_HOST,
+//     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+//     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+//     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+//     enabledTransports: ['ws', 'wss'],
+// });
+
+// export {};
+
+window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    key: 'nyk0xsrg15uvwneyfbgp',
+    wsHost: 'real-time-chat-app-production-0493.up.railway.app',
+    wsPort: 443,
+    wssPort: 443,
+    forceTLS: true,
     enabledTransports: ['ws', 'wss'],
 });
-
 export {};
